2ab89e839173ab9b14ce3c0d05344a484c45bde0
Bryce <41706121+ThinPotato@users.noreply.github.com>
2022-02-23 21:36:25 -0500

    finished unit 2 HW
    Mostly smooth. For some reason my code is not changing size of grid view.

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Bryce Stoker on 2/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
